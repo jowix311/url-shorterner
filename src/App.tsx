d9b0984cd80973580d502768d1b434a9b18d0f16
jowix311 <jowix311@gmail.com>
2025-03-15 20:43:55 +0800

    Added images to assets and initial component for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-dark-violet font-bold underline">Hello world!</h1>
+      <h2 className="text-cyan font-bold underline">hello world</h2>
     </>
   );
 }
